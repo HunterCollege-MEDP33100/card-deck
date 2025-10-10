5a4ce913f3d774628001f556c85747ce607afcb6
Ivy <ivyzhao335@gmail.com>
2025-10-09 22:25:42 -0400

    update

---
+++ displayData.js
@@ -0,0 +1,1 @@
+const cardDeck = document.querySelector('')
---
+++ fetchData.js
@@ -0,0 +1,25 @@
+function getCardData() {
+    fetch('data.json')
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(data => {
+            console.log(data);
+            displayData(data);
+        })
+}
+
+async function getCardDataAsync() {
+    const response = await fetch('data.json')
+    const data = await response.json()
+    console.log(data)
+    displayData(data)
+}
+
+function displayData(data) {
+    for (let i = 0; i < data.length; i++) {
+        const el = document.createElement('div')
+        let card1;
+    }
+}
