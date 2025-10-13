fce73cc7477ed05000bbd64df89443f0cd4d5c0d
Tiqon Lediard Jr. <lediardtiqon@gmail.com>
2025-10-13 19:52:24 -0400

    III

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const containerElement = document.querySelector('.container');
 
 class Card {
     // your code goes here
-    constructor() {
-        
+    constructor(id, name, types, lore, image) {
+        this.id = id;
+        this.name = name;
+        this.types = types;
+        this.lore = lore;
+        this.image = image;
     }
 
     // add your class functions here
@@ -12,10 +16,41 @@ class Card {
 
 function getData() {
     // your code goes here
+    fetch('data.json')
+        // turn the json file into a javascript object
+        .then(function(response) {
+            return response.json();
+        })
+        // do stuff with the javascript object
+        .then(data => {
+            console.log(data);
+            displayCards(data);
+        });
 }
 
 function displayCards(cards) {
     // your code goes here
+    this.element.innerHTML = '';
+    
+    const titleEl = document.createElement('p');
+    titleEl.classList.add('book_title');
+    titleEl.innerText = this.title;
+    this.element.appendChild(titleEl);
+    // console.log(titleEl);
+
+    const authorEl = document.createElement('p');
+    authorEl.classList.add('book_author');
+    authorEl.innerText = this.author;
+    this.element.appendChild(authorEl);
+    // console.log(authorEl);
+
+    if (this.numCopies) {
+        const numCopiesEl = document.createElement('p');
+        numCopiesEl.classList.add('book_num_copies');
+        numCopiesEl.innerText = this.numCopies;
+        // console.log(numCopiesEl);
+        this.element.appendChild(numCopiesEl);
+    }
 }
 
 // call your functions here
