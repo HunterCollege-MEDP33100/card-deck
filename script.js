993e47cc7f81615dd94150d83d9bcec545d7db12
isabell-lee08 <isabellleee8@gmail.com>
2025-10-14 18:13:06 -0400

    script and css

--- script.js
+++ script.js
@@ -2,24 +2,56 @@
 const containerElement = document.querySelector('.container');
 
 class Card {
-    // your code goes here
-    constructor(code, name, image) {
+    constructor(element, code, name, image) {
+        this.element = element;
+        
+        this.image = image;
         this.code = code;
         this.name = name;
-        this.image = image;
+
+        this.element.classList.add('card');
     }
 
-    // add your class functions here
+    displayCards() {
+        const imageEl = document.createElement('p');
+        imageEl.classList.add('image');
+        imageEl.innerText = this.image;
+        this.element.appendChild(imageEl);
+        
+        const pantoneLabel = document.createElement('p');
+        pantoneLabel.innerText = "PANTONE";
+        this.element.appendChild(pantoneLabel);
+
+        const codeEl = document.createElement('p');
+        codeEl.classList.add('code');
+        codeEl.innerText = this.code;
+        this.element.appendChild(codeEl);
+
+        const nameEl = document.createElement('p');
+        nameEl.classList.add('name');
+        nameEl.innerText = this.name;
+        this.element.appendChild(nameEl);
+    }
 }
 
-function getData() {
-    // your code goes here
+function getCardData() {
+    fetch('data.json')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            displayData(data);
+        });
 }
 
-function displayCards(cards) {
-    // your code goes here
+function displayData(data) {
+    for (let i = 0; i < data.length; i++) {
+        const cardElement = document.createElement('div');
+        const card1 = new Card(cardElement, data[i].code, data[i].name, data[i].image);
+        card1.displayCards();
+        containerElement.appendChild(cardElement);
+    }
 }
 
-// call your functions here
-
-
+getCardData();
